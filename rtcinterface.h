38a2355e2a42c4e7b3b5a2931c07472fb35f0248
cheng_lu <5693806+cheng_lu_gitee@user.noreply.gitee.com>
2024-09-11 06:19:52 +0000

    update rtcinterface.h. publishAuditStream->publishAudioStream
    Signed-off-by: cheng_lu <5693806+cheng_lu_gitee@user.noreply.gitee.com>

--- rtcinterface.h
+++ rtcinterface.h
@@ -19,7 +19,7 @@ public:
     virtual int sendPrivateMessage(int msgType, std::string message, std::string targetUserId) = 0;
     virtual int sendPublicMessage(int msgType, std::string message) = 0;
 
-    virtual int publishAuditStream(char *data, int len) = 0;
+    virtual int publishAudioStream(char *data, int len) = 0;
     virtual int publishVedioStream(int w, int h, char *data, int len) = 0;
 
     virtual int subAudioStream(const std::string &targetUserId) = 0;
